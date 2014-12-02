f1b4f75e9373caa19c934a3339d9716554b47c33
Ravish Tiwari <ravishktiwari@hotmail.com>
2014-12-02 17:04:03 +0530

    Gulp for builds

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+/* jshint node:true */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var $ = require('gulp-load-plugins')();
+// var DIST_DIR = "dist";
+
+gulp.task('styles', function () {
+  return gulp.src('src/assets/css/main.scss')
+    .pipe(sass({
+      style: 'expanded',
+      precision: 10
+    }))
+    .pipe(gulp.dest('src/assets/css/'))
+    .pipe(gulp.dest('dist/assets/css/'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('src/assets/js/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('default'))
+    .pipe($.notify({ message: 'JS Hinting task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/assets/js/**/*.js')
+      .pipe(gulp.dest('dist/assets/js'))
+      .pipe($.notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('html', ['styles'], function () {
+  var assets = $.useref.assets({searchPath: '{.tmp,src}'});
+  return gulp.src('src/*.html')
+    .pipe(assets)
+    .pipe(assets.restore())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('src/assets/img/**/*')
+    .pipe(gulp.dest('dist/assets/img'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('src/assets/fonts/**/*')
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe(gulp.dest('dist/assets/fonts'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('connect', ['styles'], function () {
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+  var app = require('connect')()
+    .use(require('connect-livereload')({port: 35729}))
+    .use(serveStatic('.tmp'))
+    .use(serveStatic('src'))
+    .use(serveIndex('src'));
+  require('http').createServer(app)
+  .listen(9000)
+  .on('listening', function () {
+    console.log('Started connect web server on http://localhost:9000');
+  });
+});
+
+gulp.task('serve', ['connect', 'watch'], function () {
+  require('opn')('http://localhost:9000');
+});
+
+gulp.task('watch',['jshint','connect'], function() {
+  gulp.watch('./src/assets/css/**/*.scss', ['styles']);
+  gulp.watch('./src/assets/js/*.js', ['jshint', 'scripts']);
+  gulp.watch('./src/assets/img/**/*', ['images']);
+  var server = $.livereload();
+  gulp.watch(['./src/**']).on('change', server.changed);
+  $.livereload.listen();
+});
+
+gulp.task('build', ['jshint','scripts', 'html', 'images', 'fonts','styles'], function () {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build'}));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
